24fcbc02b910eb64cf8161f45bd8d582221f67be
Jeremy Nelson <jermnelson.gmail.com>
2013-09-25 10:45:41 -0600

    updated to include new views and logic for presentation

--- web.py
+++ web.py
@@ -3,9 +3,22 @@ __author__ = "Jeremy Nelson"
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+SLIDES = {'bibframe':[{'name': 'history',
+                       'label': 'History of BIBFRAME'}],
+          'rda': [{'name': 'aacrl2-and-rda',
+                   'label': 'AACRL2 and RDA'},
+                  {'name': 'marc21-and-BIBFRAME',
+                   'label': 'MARC21 and BIBFRAME'},
+                  {'name': 'rda-in-bibframe',
+                   'label': 'RDA in BIBFRAME'}]}
+
+URL_PREFIX = '/calcon-2013-session'
+
+@app.route('{0}/'.format(URL_PREFIX))
 def home():
-    return render_template('layout.html')
+    return render_template('index.html',
+                           category='home',
+                           slides=SLIDES)
 
 if __name__ == '__main__':
     app.run(debug=True)
