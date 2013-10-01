ec4f6d94e4c71c7dd77d713033a8525201f8cc7f
Jeremy Nelson <jermnelson.gmail.com>
2013-10-01 15:34:06 -0600

    Updated to use flup

--- web.py
+++ web.py
@@ -1,11 +1,62 @@
-__author__ = "Jeremy Nelson"
+"""
+Module for the CALCON 2013 RDF & BIBFRAME Session Presentation
 
+Copyright (C) 2013 Jeremy Nelson 
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+__version_info__ = ('0', '0', '5')
+__version__ = '.'.join(__version_info__)
+__author__ = "Jeremy Nelson"
+__license__ = 'GPL v2'
+__copyright__ = '(c) 2013 by Jeremy Nelson'
+
+import argparse
 import json
 
-from flask import Flask, render_template, url_for
+from flask import Flask, redirect, render_template, url_for
 from flask import jsonify, request, session
+from flask.ext.login import LoginManager, login_user, login_required
+from flask.ext.login import make_secure_token, UserMixin
+from flask_oauth import OAuth
+
+from flup.server.fcgi import WSGIServer
+
+GOOGLE_SETTINGS = json.load(open('google_auth.json', 'rb'))
+
+oauth = OAuth()
+
+google = oauth.remote_app(
+    'google',
+    base_url='https://www.google.com/accounts',
+    authorize_url=GOOGLE_SETTINGS['web'].get('auth_uri'),
+    request_token_url=None,
+    request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                          'response_type': 'code'},
+    access_token_url=GOOGLE_SETTINGS['web'].get('token_uri'),
+    access_token_method='POST',
+    access_token_params={'grant-type': 'authorization_code'},
+    consumer_key=GOOGLE_SETTINGS['web'].get('client_id'),
+    consumer_secret=GOOGLE_SETTINGS['web'].get('client_secret'))
+    
+    
+                          
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 ANSWERS = json.load(open('answers.json', 'rb'))
 app.secret_key = ANSWERS.pop('secret_key')
@@ -32,6 +83,28 @@ SLIDES = {'bibframe':[{'name': 'history',
                    'label': 'BIBFRAME and the Redis Library Services Platform'}]}
 
 URL_PREFIX = '/calcon-2013-session'
+
+class User(UserMixin):
+
+    def __init__(self, email, id, active=True):
+        self.id = id
+        self.email = email
+        self.active = active
+
+    def get_id(self):
+        return self.id
+
+    def is_active(self):
+        return self.active
+
+    def get_auth_token(self):
+        return make_secure_token(self.email,
+                                 key='deterministic')
+        
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
 
 @app.route('{0}/badge.html'.format(URL_PREFIX))
 def badge():
@@ -71,6 +144,19 @@ def grade():
                     slide,
                     session[slide])})
 
+
+
+@app.route('{0}/login'.format(URL_PREFIX),
+       methods=['GET', 'POST'])
+def login():
+    return google.authorize(callback='http://tuttdemo.coloradocollege.edu/')
+##    if request.method == 'POST':
+##        
+##        return redirect(request.args.get("next") or url_for("home"))
+##    return render_template("login.html",
+##                           category='home',
+##                           slides=SLIDES)
+    
                            
 @app.route('{0}/resources.html'.format(URL_PREFIX))
 def resources():
@@ -120,4 +206,20 @@ def home():
                            slides=SLIDES)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    parser = argparse.ArgumentParser(
+        description='Run CALCON 2013 Session Presentation')
+    parser.add_argument('mode',
+                    help='Run in either prod (production) or dev (development)')
+    mode = parser.parse_args().mode
+    host = '0.0.0.0'
+    port = 8013
+    if mode == 'prod':
+        WSGIServer(app,
+                   bindAddress='{0}:{1}'.format(host,
+                                                port)).run()
+    elif mode == 'dev':
+        app.run(host=host,
+                port=port,
+                debug=True)
+    else:
+        print("Unknown mode {0}, should be prod or dev".format(mode))
